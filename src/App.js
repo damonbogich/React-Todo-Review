709f590d8026e9a37e6967c5bc960f2c1ecd6b6c
damonbogich <damonbogich@gmail.com>
2020-11-30 18:26:32 -0500

    add item working

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ class App extends React.Component {
           id: 2,
           completed: false
         }
-      ]
+      ], 
+      taskName: ""
     }
   }
   handleCompletion = (id) => {
@@ -38,15 +39,36 @@ class App extends React.Component {
     // needs to access state and remove all items from state that are marked as completed
     let data = this.state.data.filter(task => task.completed !== true)
     //set state to new data array
+    console.log(data)
+    
     this.setState({data})
+    console.log(this.state)
   }
+
+  addToDo = (string) => {
+    //function needs to add a to do to data array in state 
+    //This will be an input from user (task)
+    //The id must be auto created and the completed prop must be false 
+    string = this.state.taskName;
+    let newTask = {task: string, id: Date.now(), completed: false};
+    this.setState(state => {
+      const data = this.state.data.push(newTask);
+      return data
+    })
+  }
+
+  handleInput = e => {
+   this.setState({taskName: e.target.value})
+   console.log(this.state.taskName)
+  }
+  
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoList handleCompletion={this.handleCompletion} items={this.state.data}/>
-        <TodoForm handleClearCompleted={this.handleClearCompleted} />
+        <TodoForm newTask={this.state.newTask} addToDo={this.addToDo} handleInput={this.handleInput} addToDo={this.addToDo} handleClearCompleted={this.handleClearCompleted} />
       </div>
     );
   }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const TodoForm = props => {
+    let newTask = props.newTask
     return (
         <div>
-            <input placeholder= "addtodo" />
-            <button>Add Todo</button>
+            <input onChange={e => props.handleInput(e)} placeholder= "add a task"/>
+            <button onClick={() => props.addToDo(newTask)}>Add Todo</button>
             <button onClick={() => props.handleClearCompleted()}>Clear Completed</button>
         </div>
     )
